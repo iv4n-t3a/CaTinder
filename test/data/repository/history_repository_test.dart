14a692b5d84b30b29bb029989d2fed4d8c7cf727
iv4n-t3a <iv4n.t3a@yandex.ru>
2025-05-08 23:56:32 +0300

    Some fixess

--- test/data/repository/history_repository_test.dart
+++ test/data/repository/history_repository_test.dart
@@ -1,12 +1,10 @@
+import 'package:app/data/history/repository.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get_it/get_it.dart';
-import 'package:mockito/mockito.dart';
 import 'package:app/domain/entities/cat.dart';
-import 'package:app/domain/repositories/history.dart';
 
 void main() {
   late Cat testCat;
-  late HistoryRepository testedRepo;
+  late HistoryRepositoryImpl testedRepo = HistoryRepositoryImpl();
 
   setUp(() {
     testCat = Cat(
@@ -17,28 +15,10 @@ void main() {
       origin: "f",
       imageUrl: "g",
     ); // Example Cat object
-    testedRepo = GetIt.I.get<HistoryRepository>();
-  });
-
-  test('addLike should call addLike on the repository', () async {
-    when(testedRepo.addLike(testCat)).thenAnswer((_) async {});
-
-    await testedRepo.addLike(testCat);
-
-    verify(testedRepo.addLike(testCat)).called(1);
-  });
-
-  test('removeLike should call removeLike on the repository', () async {
-    when(testedRepo.removeLike(testCat)).thenAnswer((_) async {});
-
-    await testedRepo.removeLike(testCat);
-
-    verify(testedRepo.removeLike(testCat)).called(1);
   });
 
   test('likes should return a list of liked cats', () async {
     final likedCats = [testCat];
-    when(testedRepo.likes).thenAnswer((_) async => likedCats);
 
     final result = await testedRepo.likes;
 
@@ -47,7 +27,6 @@ void main() {
 
   test('breeds should return a list of cat breeds', () async {
     final breeds = ['Siamese', 'Persian'];
-    when(testedRepo.breeds).thenAnswer((_) async => breeds);
 
     final result = await testedRepo.breeds;
 
@@ -56,8 +35,6 @@ void main() {
 
   test('getLikeDate should return the like date of a cat', () async {
     final likeDate = DateTime.now();
-    when(testedRepo.getLikeDate(testCat))
-        .thenAnswer((_) async => likeDate);
 
     final result = await testedRepo.getLikeDate(testCat);
 
@@ -66,7 +43,6 @@ void main() {
 
   test('likesCount should return the number of likes', () async {
     const count = 5;
-    when(testedRepo.likesCount).thenAnswer((_) async => count);
 
     final result = await testedRepo.likesCount;
 
